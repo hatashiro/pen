4b837c24f7a2e46c5c3660f5dfd9201ef9045963
Jun <me@noraesae.net>
2017-01-14 17:18:15 +0900

    Update frontend main to work with static HTML
    Add Markdown div and set title dinamically.

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -6,7 +6,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HTMLRenderer from './html-renderer';
 
+const app = document.createElement('div');
+app.setAttribute('id', 'app');
+app.setAttribute('class', 'markdown-body');
+document.body.appendChild(app);
+
+// Set title to Markdown filename
+const pathTokens = location.pathname.split('/');
+document.title = pathTokens[pathTokens.length - 1];
+
 ReactDOM.render(
   React.createElement(HTMLRenderer, { location }),
-  document.getElementById('app')
+  app
 );
